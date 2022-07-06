2ef2c11a6b8fa47baf5f54489bc40c78e699eb92
william flinchbaugh <Williamflinchbaugh@gmail.com>
2022-07-06 18:46:07 -0500

    fixing obs space and angles and spawn points

--- PPO_train.py
+++ PPO_train.py
@@ -31,15 +31,15 @@ saved_timesteps = timesteps // save_freq * save_freq
 
 # ---------- TRAINING ----------
 callback = TrainAndLoggingCallback(check_freq=save_freq, save_path=CHECKPOINT_DIR)
-env = BattleEnvironment(show=False, hit_base_reward=10, hit_plane_reward=10, miss_punishment=-1, too_long_punishment=0, closer_to_base_reward=0, 
-    closer_to_plane_reward=0, lose_punishment=-10)
+env = BattleEnvironment(show=False, hit_base_reward=20, hit_plane_reward=20, miss_punishment=-1, too_long_punishment=0, closer_to_base_reward=0, 
+    closer_to_plane_reward=0, lose_punishment=-15)
 config = [env.hit_base_reward, env.hit_plane_reward, env.miss_punishment, env.too_long_punishment, env.closer_to_base_reward, env.closer_to_plane_reward, env.lose_punishment]
-# model = PPO('MlpPolicy', env, tensorboard_log=LOG_DIR, verbose=1)
-# model.learn(total_timesteps=timesteps, callback=callback)
-# del model
+model = PPO('MlpPolicy', env, tensorboard_log=LOG_DIR, verbose=1)
+model.learn(total_timesteps=timesteps, callback=callback)
+del model
 
 # # ---------- EVALUATION ----------
-# model = PPO.load(f'{CHECKPOINT_DIR}best_model_{saved_timesteps}')
+model = PPO.load(f'{CHECKPOINT_DIR}best_model_{saved_timesteps}')
 env = BattleEnvironment(show=True, hit_base_reward=config[0], hit_plane_reward=config[1], miss_punishment=config[2], too_long_punishment=config[3], closer_to_base_reward=config[4], 
     closer_to_plane_reward=config[5], lose_punishment=config[6], fps=30)
 episodes = 1000
@@ -48,8 +48,8 @@ for episode in range(episodes): # Evaluates the model n times
     score = 0
     while not env.done:
         env.render()
-        # action, _states = model.predict(state)
-        action = env.action_space.sample()
+        action, _states = model.predict(state)
+        # action = env.action_space.sample()
         n_state, reward, done, info = env.step(action)
         score += reward
     if (episode+1) % 10 == 0:
@@ -59,17 +59,17 @@ with open(f'{FOLDER}/results.txt', 'w') as f:
     print(f"VALUES:\nhit base:{config[0]}\nhit plane:{config[1]}\nmiss:{config[2]}\ntoo long:{config[3]}\ncloser to base:{config[4]}\ncloser to plane:{config[5]}\nlose:{config[6]}", file=f)
     print(env.wins(), file=f)
 
-# ---------- VISUALIZATION ----------
-# env = BattleEnvironment(show=True, hit_base_reward=config[0], hit_plane_reward=config[1], miss_punishment=config[2], too_long_punishment=config[3], closer_to_base_reward=config[4], 
-#     closer_to_plane_reward=config[5], lose_punishment=config[6], fps=60)
-# episodes = 5
-# for episode in range(episodes): # Evaluates the model n times
-#     state = env.reset()
-#     score = 0
-#     while not env.done:
-#         env.render()
-#         action = env.action_space.sample()
-#         n_state, reward, done, info = env.step(action)
-#         score += reward
-#     print('Episode:{} Score:{}'.format(episode+1, score))
-# print(env.wins())
+#---------- VISUALIZATION ----------
+env = BattleEnvironment(show=True, hit_base_reward=config[0], hit_plane_reward=config[1], miss_punishment=config[2], too_long_punishment=config[3], closer_to_base_reward=config[4], 
+    closer_to_plane_reward=config[5], lose_punishment=config[6], fps=60)
+episodes = 5
+for episode in range(episodes): # Evaluates the model n times
+    state = env.reset()
+    score = 0
+    while not env.done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
+    print('Episode:{} Score:{}'.format(episode+1, score))
+print(env.wins())
