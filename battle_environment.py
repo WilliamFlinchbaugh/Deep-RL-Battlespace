9d653dd1cc53d16754604f89a751018f71c799ed
william flinchbaugh <Williamflinchbaugh@gmail.com>
2022-07-13 18:23:27 -0500

    20% done

--- battle_environment.py
+++ battle_environment.py
@@ -9,8 +9,10 @@ import gym
 from gym import spaces
 import os
 import sys
-from stable_baselines3.common import env_checker
-from stable_baselines3 import PPO
+import functools
+from pettingzoo import AECEnv
+from pettingzoo.utils import agent_selector
+from pettingzoo.utils import wrappers
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 WHITE = (255, 255, 255)
@@ -19,6 +21,17 @@ BLUE = (0, 93, 135)
 BLACK = (0, 0, 0)
 DISP_WIDTH = 1000
 DISP_HEIGHT = 1000
+
+def env():
+    env = raw_env()
+    # This wrapper is only for environments which print results to the terminal
+    env = wrappers.CaptureStdoutWrapper(env)
+    # this wrapper helps error handling for discrete action spaces
+    env = wrappers.AssertOutOfBoundsWrapper(env)
+    # Provides a wide vareity of helpful user errors
+    # Strongly recommended
+    env = wrappers.OrderEnforcingWrapper(env)
+    return env
 
 # ---------- HELPER FUNCTIONS -----------
 def rel_angle(p0, a0, p1):
@@ -232,40 +245,55 @@ class Bullet(pygame.sprite.Sprite):
         return self.direction
 
 # ----------- BATTLE ENVIRONMENT -----------
-class BattleEnvironment(gym.Env):
-    def __init__(self, show=False, hit_base_reward=2, hit_plane_reward=1, miss_punishment=0, too_long_punishment=0, lose_punishment=-2, fps=60):
+class BattleEnvironment(AECEnv):
+    def __init__(self, config, n_agents):
         super(BattleEnvironment, self).__init__()
 
-        self.width = DISP_WIDTH
-        self.height = DISP_HEIGHT
-        self.max_time = 10
-        self.action_space = spaces.Discrete(4) # Forward, Shoot, Turn right, Turn left (+ Turn to base and turn to plane for random agent)
-
-        # ---------- Observation Space ----------
-        obs_space = {
-            'fplane_dist_oplane': spaces.Box(-1, 1, dtype=np.float32, shape=(1,)),
-            'fplane_dist_obase': spaces.Box(-1, 1, dtype=np.float32, shape=(1,)),
-            'fplane_angle_oplane': spaces.Box(-1, 1, dtype=np.float32, shape=(1,)),
-            'fplane_angle_obase': spaces.Box(-1, 1, dtype=np.float32, shape=(1,))
-        }   
-
-        mins = np.array([x.low[0] for x in obs_space.values()])
-        maxs = np.array([x.high[0] for x in obs_space.values()])
-
-        self.observation_space = spaces.Box(mins, maxs, dtype=np.float32)
-        
-        # ---------- Initialize values ----------
         self.team = {}
         self.team['red'] = {}
         self.team['blue'] = {}
         self.team['red']['base'] = Base('red')
         self.team['blue']['base'] = Base('blue')
         self.team['red']['planes'] = []
-        self.team['red']['planes'].append(Plane('red'))
         self.team['blue']['planes'] = []
-        self.team['blue']['planes'].append(Plane('blue'))
         self.team['red']['wins'] = 0
         self.team['blue']['wins'] = 0
+
+        self.possible_agents = []
+        self.n_agents = n_agents
+
+        for i in range(n_agents):
+            self.team['red']['planes'].append(Plane('red'))
+            self.possible_agents.append(self.team['red']['planes'][i])
+        for i in range(n_agents):
+            self.team['blue']['planes'].append(Plane('blue'))
+            self.possible_agents.append(self.team['blue']['planes'][i])
+
+        self.agent_name_mapping = dict(
+            zip(self.possible_agents, list(range(len(self.possible_agents))))
+        )
+
+        obs = {} # Observation per agent, consists of dist + angle of each enemy, and base
+
+        obs['base_dist'] = spaces.Box(-1, 1, dtype=np.float32, shape=(1,))
+        obs['base_angle'] = spaces.Box(-1, 1, dtype=np.float32, shape=(1,))
+        for i in range(n_agents):
+            obs[f'plane{i}_dist'] = spaces.Box(-1, 1, dtype=np.float32, shape=(1,))
+            obs[f'plane{i}_angle'] = spaces.Box(-1, 1, dtype=np.float32, shape=(1,))
+
+        mins = np.array([x.low[0] for x in obs.values()])
+        maxs = np.array([x.high[0] for x in obs.values()])
+
+        obs_space = spaces.Box(mins, maxs, dtype=np.float32)
+
+         # Forward, Shoot, Turn right, Turn left
+        self._observation_spaces = {agent: obs_space for agent in self.possible_agents}
+        self._action_spaces = {agent: spaces.Discrete(4) for agent in self.possible_agents}
+        
+        # ---------- Initialize values ----------
+        self.width = DISP_WIDTH
+        self.height = DISP_HEIGHT
+        self.max_time = 10
         self.total_games = 0
         self.ties = 0
         self.bullets = []
@@ -273,43 +301,45 @@ class BattleEnvironment(gym.Env):
         self.bullet_speed = 400 # mph
         self.total_time = 0 # in hours
         self.time_step = 0.1 # hours per time step
-        self.show = show # show the pygame animation
         self.step_turn = 20 # degrees to turn per step
-        self.hit_base_reward = hit_base_reward
-        self.hit_plane_reward = hit_plane_reward
-        self.miss_punishment = miss_punishment
-        self.too_long_punishment = too_long_punishment
-        self.lose_punishment = lose_punishment
-        self.fps = fps
-        self.blue_ai = PPO.load("models/PPO_1/final_model", env=self)
+        self.show = config['show'] # show the pygame animation
+        self.hit_base_reward = config['hit_base_reward']
+        self.hit_plane_reward = config['hit_plane_reward']
+        self.miss_punishment = config['miss_punishment']
+        self.lose_punishment = config['lose_punishment']
+        self.fps = config['fps']
     
-    def _get_observation(self, fteam):
-        fplane = self.team[fteam]['planes'][0]
-        oteam = 'blue' if fteam == 'red' else 'red'
-        oplane = self.team[oteam]['planes'][0]
+    def observe(self, agent):
+        oteam = 'blue' if agent.team == 'red' else 'red'
+        agent_plane = self.team[agent.team]['planes'][int(agent[-1])]
+        agent_pos = agent_plane.get_pos()
+        agent_dir = agent_plane.get_direction()
+        oplanes = self.team[oteam]['planes']
         obase = self.team[oteam]['base']
-
-        fplane_pos = fplane.get_pos()
-        fplane_direction = fplane.get_direction()
-        oplane_pos = oplane.get_pos()
         obase_pos = obase.get_pos()
 
-        fplane_dist_oplane = dist(fplane_pos, oplane_pos) / (math.sqrt(math.pow(self.width, 2) + math.pow(self.height, 2))) * 2 - 1
-        fplane_dist_obase = dist(fplane_pos, obase_pos) / (math.sqrt(math.pow(self.width, 2) + math.pow(self.height, 2))) * 2 - 1
-        rel_angle_oplane = rel_angle(fplane_pos, fplane_direction, oplane_pos) / 360
-        rel_angle_obase = rel_angle(fplane_pos, fplane_direction, obase_pos) / 360
+        dct = {}
+        dct['base_dist'] = dist(agent_pos, obase_pos) / (math.sqrt(math.pow(self.width, 2) + math.pow(self.height, 2))) * 2 - 1
+        dct['base_angle'] = rel_angle(agent_pos, agent_dir, obase_pos) / 360
+        for i in range(oplanes):
+            plane_pos = oplanes[i].get_pos()
+            dct[f'plane{i}_dist'] = dist(agent_pos, plane_pos) / (math.sqrt(math.pow(self.width, 2) + math.pow(self.height, 2))) * 2 - 1
+            dct[f'plane{i}_angle'] = rel_angle(agent_pos, agent_dir, plane_pos) / 360
 
-        dct = {
-            'fplane_dist_oplane': fplane_dist_oplane,
-            'fplane_dist_obase': fplane_dist_obase,
-            'fplane_angle_oplane': rel_angle_oplane,
-            'fplane_angle_obase': rel_angle_obase
-        }   
-        
         return np.array([x for x in dct.values()], dtype=np.float32)
 
-    def reset(self): # return observation
-        self.done = False
+    def reset(self):
+        self.agents = self.possible_agents[:]
+        self.rewards = {agent: 0 for agent in self.agents}
+        self._cumulative_rewards = {agent: 0 for agent in self.agents}
+        self.dones = {agent: False for agent in self.agents}
+        self.infos = {agent: {} for agent in self.agents}
+        self.state = {agent: -1 for agent in self.agents}
+        self.observations = {agent: -1 for agent in self.agents}
+        self.num_moves = 0
+        self._agent_selector = agent_selector(self.agents)
+        self.agent_selection = self.agent_selector.next()
+
         self.winner = 'none'
 
         self.team['red']['base'].reset()
@@ -330,11 +360,19 @@ class BattleEnvironment(gym.Env):
             self.display = pygame.display.set_mode((DISP_WIDTH, DISP_HEIGHT))
             pygame.display.set_caption("Battlespace Simulator")
             pygame.time.wait(1000)
-        return self._get_observation('red')
 
     def step(self, action): # return observation, reward, done, info
-        reward = 0
+        if self.dones[self.agent_selection]: # Checks if agent is already done
+            return self._was_done_step(action)
 
+        agent = self.agent_selection
+
+        self._cumulative_rewards[agent] = 0
+        self.state[self.agent_selection] = action
+
+        # Collect reward if it is the last agent to step
+        if self._agent_selector.is_last():
+            self.rewards[self.agents[0]]
         # Check if over time, if so, end game in tie
         self.total_time += self.time_step
         if self.total_time >= self.max_time:
@@ -477,6 +515,10 @@ class BattleEnvironment(gym.Env):
     def wins(self):
         return "Wins by red: {}\nWins by blue: {}\nTied games: {}\nWin rate: {}".format(self.team['red']['wins'], self.team['blue']['wins'], self.ties, self.team['red']['wins']/self.total_games)
 
+    def close(self):
+        pygame.quit()
+        sys.exit()
+
     def render(self, mode="human"):
         if self.show: # Just to ensure it won't render if self.show == False
             for event in pygame.event.get():
@@ -484,13 +526,10 @@ class BattleEnvironment(gym.Env):
                 if event.type == KEYDOWN:
                     # If the Esc key is pressed, then exit the main loop
                     if event.key == K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
+                        self.close()
                 # Check for QUIT event. If QUIT, then set running to false.
                 elif event.type == QUIT:
-                    pygame.quit()
-                    self.done = True
-                    sys.exit()
+                    self.close()
                     
             font = pygame.font.Font('freesansbold.ttf', 12)
 
