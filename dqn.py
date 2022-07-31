716f40bf43d17f5ec867b6fa79e0b5dadbdc6569
BallpointPenBoi <WilliamFlinchbaugh@gmail.com>
2022-07-30 19:32:25 -0500

    added param for using gpu

--- dqn.py
+++ dqn.py
@@ -7,7 +7,7 @@ import battle_v1
 
 class DeepQNetwork(nn.Module):
     def __init__(self, lr, input_dims, fc1_dims, fc2_dims,
-                 n_actions):
+                 n_actions, use_gpu=True):
         super(DeepQNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
@@ -19,11 +19,12 @@ class DeepQNetwork(nn.Module):
 
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
-        if T.cuda.is_available:
-            print("\n - Using GPU because available - \n")
+        device = 'cuda:0' if T.cuda.is_available() and use_gpu else 'cpu'
+        self.device = T.device(device)
+        if device == 'cuda:0':
+            print("\n - Using GPU - \n")
         else:
-            print("\n - Using CPU because GPU not available - \n")
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+            print("\n - Using CPU - \n")
         self.to(self.device)
 
     def forward(self, state):
@@ -35,7 +36,7 @@ class DeepQNetwork(nn.Module):
 
 class Agent:
     def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,
-                 max_mem_size=100000, eps_end=0.05, eps_dec=5e-4):
+                 max_mem_size=100000, eps_end=0.05, eps_dec=5e-4, use_gpu=True):
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_min = eps_end
@@ -49,12 +50,9 @@ class Agent:
         self.replace_target = 100
 
         self.Q_eval = DeepQNetwork(lr, n_actions=n_actions,
-                                   input_dims=input_dims,
-                                   fc1_dims=256, fc2_dims=256)
-        self.state_memory = np.zeros((self.mem_size, *input_dims),
-                                     dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims),
-                                         dtype=np.float32)
+                    input_dims=input_dims, fc1_dims=256, fc2_dims=256, use_gpu=use_gpu)
+        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
@@ -136,10 +134,12 @@ BATCH_SIZE = 64
 env = battle_v1.parallel_env(**cf)
 n_actions = env.n_actions
 
+use_gpu = True
+
 agents = {}
 for agent_id in env.possible_agents:
     agents[agent_id] = Agent(GAMMA, EPS_START, LEARNING_RATE, [env.obs_size], 
-                            BATCH_SIZE, n_actions, eps_end=EPS_END, eps_dec=EPS_DEC)
+                BATCH_SIZE, n_actions, eps_end=EPS_END, eps_dec=EPS_DEC, use_gpu=use_gpu)
 
 n_games = 100000
 game_cntr = 0
