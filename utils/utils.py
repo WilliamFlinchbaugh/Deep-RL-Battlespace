a1a00396d63c3d21cd8652a90613707a1e00b87d
BallpointPenBoi <WilliamFlinchbaugh@gmail.com>
2022-10-08 20:49:50 -0500

    fixed overloaded function

--- utils/utils.py
+++ utils/utils.py
@@ -34,12 +34,12 @@ def plot_data(score_dict, filename):
         games_played = len(score_dict[team])
         running_avg = np.empty(games_played)
         for t in range(games_played):
-            running_avg[t] = np.mean(scores[max(0, t-5):(t+1)])
+            running_avg[t] = np.mean(score_dict[team][max(0, t-5):(t+1)])
         x = [i for i in range(games_played)]
         cubic_interploation_model = interp1d(x, running_avg, kind = "cubic")
         X_=np.linspace(min(x), max(x), 30)
         Y_=cubic_interploation_model(X_)
-        ax.plot(X_, Y_, team, label=id)
+        ax.plot(X_, Y_, team, color=team, label=team)
 
     ax.set_title('Average Score for Each Team')
     ax.set_xlabel('Number of games played')
