908c97961541009b6ab375574576621b63b71cc9
BallpointPenBoi <williamflinchbaugh@gmail.com>
2022-11-17 09:05:31 -0600

    restructured hyperparamters and added menu

--- main.py
+++ main.py
@@ -7,64 +7,100 @@ import os
 import datetime
 from utils.utils import plot_data
 import sys
+import json
+import shutil
+
+hyperparams = {
+    'gamma': 0.95,
+    'lr': 0.0001,
+    'buffer_size': 100_000,
+    'batch_size': 1024,
+    'print_interval': 100,
+    'save_interval': 100,
+    'learn_interval': 20,
+    'render_interval': 1000,
+    'max_episodes': 500_000,
+}
+
+env_config = {
+    'n_agents': 1, # Number of planes on each team
+    'show': False, # Show visuals
+    'hit_base_reward': 100, # Reward value for hitting enemy base
+    'hit_plane_reward': 100, # Reward value for hitting enemy plane
+    'miss_punishment': 0, # Punishment value for missing a shot
+    'die_punishment': 0, # Punishment value for a plane dying
+    'lose_punishment': 0, # Punishment for losing the game (The goal is to possibly defend the base)
+    'fps': 20, # Framerate that the visuals run at
+    'continuous_actions': True
+}
 
 def merge_dicts(dict1, dict2):
     dict2.update(dict1)
     return dict2
 
-GAMMA = 0.95
-LEARNING_RATE = 0.01
-BUFFER_SIZE = 100_000
-BATCH_SIZE = 1_024
-PRINT_INTERVAL = 100
-SAVE_INTERVAL = 100
-LEARN_INTERVAL = 10
-SHOW_INTERVAL = 1_000
-N_GAMES = 500_000
+def save_hyperparams(path, param_dict):
+    with open(path + '/hyperparams.json', 'w') as f:
+        f.write(json.dumps(param_dict))
 
-env_config = {
-    'n_agents': 2, # Number of planes on each team
-    'show': False, # Show visuals
-    'hit_base_reward': 50, # Reward value for hitting enemy base
-    'hit_plane_reward': 25, # Reward value for hitting enemy plane
-    'miss_punishment': -1, # Punishment value for missing a shot
-    'die_punishment': 0, # Punishment value for a plane dying
-    'lose_punishment': -30, # Punishment for losing the game (The goal is to possibly defend the base)
-    'fps': 30, # Framerate that the visuals run at
-    'continuous_actions': True
-}
+def save_config(path, config):
+    with open(path + '/env_config.json', 'w') as f:
+        f.write(json.dumps(config))
 
-def main():
-    # Create a new folder for the model
-    for i in range(1, 100):
-        if not os.path.exists(f'models/maddpg_{i}'):
-            FOLDER = f'models/maddpg_{i}'
-            os.makedirs(FOLDER)
-            os.makedirs(f'{FOLDER}/training_vids')
-            break
+if __name__ == '__main__':
+    # Menu
+    print('1. Override a model')
+    print('2. Continue training a model')
+    print('3. Train a new model')
+    print('4. Quit')
+    choice = input('Enter a number: ')
+    if choice == '1':
+        model_name = input('Enter model name: ')
+        model_path = f'models/{model_name}'
+        if not os.path.exists(model_path):
+            print('Model does not exist')
+            sys.exit()
+        shutil.rmtree(model_path)
+        FOLDER = model_path
+        os.makedirs(model_path)
+        os.makedirs(f'{model_path}/training_vids')
+        save_hyperparams(FOLDER, hyperparams)
+        save_config(FOLDER, env_config)
 
-    hyperparams = {
-        "gamma":GAMMA,
-        "learning_rate":LEARNING_RATE,
-        "buffer_size":BUFFER_SIZE,
-        "batch_size":BATCH_SIZE,
-        "n_games":N_GAMES
-    }
-
-    with open(f"{FOLDER}/config.txt", 'a') as f:
-        f.write("MADDPG\n\nENV CONFIG:\n")
-        pprint(env_config, stream=f)
-        f.write("\nHYPERPARAMETERS:\n")
-        pprint(hyperparams, stream=f)
+    elif choice == '2':
+        model_name = input('Enter model name: ')
+        FOLDER = f'models/{model_name}'
+        if not os.path.exists(FOLDER):
+            print('Model does not exist')
+            sys.exit()
+        with open(f'{FOLDER}/hyperparams.json', 'r') as f:
+            hyperparams = json.load(f)
+        with open(f'{FOLDER}/env_config.json', 'r') as f:
+            env_config = json.load(f)
+        hyperparams['max_episodes'] = int(input('Enter number of games to train: '))
+        save_hyperparams(FOLDER, hyperparams)
+        save_config(FOLDER, env_config)
+            
+    elif choice == '3':
+        # Create a new folder for the model
+        for i in range(1, 100):
+            if not os.path.exists(f'models/{i}'):
+                FOLDER = f'models/{i}'
+                os.makedirs(FOLDER)
+                os.makedirs(f'{FOLDER}/training_vids')
+                break
+        save_hyperparams(FOLDER, hyperparams)
+        save_config(FOLDER, env_config)
         
     env = battle_env.parallel_env(**env_config)
+
     red_agent_list = env.possible_red
     blue_agent_list = env.possible_blue
+
     obs_len = env.observation_space(red_agent_list[0]).shape[0]
     critic_dims = obs_len * env.n_agents
-    red_team = maddpg.Team(red_agent_list, obs_len, env.n_actions, critic_dims, chkpt_dir=FOLDER, mem_size=BUFFER_SIZE, batch_size=BATCH_SIZE, lr=LEARNING_RATE)
-    blue_team = instinct.Team(blue_agent_list, red_agent_list, env)
 
+    red_team = maddpg.Team(red_agent_list, obs_len, env.n_actions, critic_dims, hyperparams['buffer_size'], hyperparams['batch_size'], hyperparams['gamma'], hyperparams['lr'], FOLDER)
+    blue_team = instinct.Team(blue_agent_list, red_agent_list, env)
 
     steps = 0
     red_scores = []
@@ -73,9 +109,8 @@ def main():
     print(f'\n{" Starting Training ":=^43}')
     start = datetime.datetime.now()
 
-    for i in range(N_GAMES+1):
-        sys.stdout.write(f"\r{' Episode {game}, %{percent:.2f} Complete '.format(game = i, percent = i / N_GAMES * 100):=^43}")
-        # Episode {i}, %{i / N_GAMES * 100:.2f} Complete
+    for i in range(hyperparams['max_episodes']+1):
+        sys.stdout.write(f"\r{' Episode {game}, %{percent:.2f} Complete '.format(game = i, percent = i / hyperparams['max_episodes'] * 100):=^43}")
         observations = env.reset()
 
         red_score = 0
@@ -88,7 +123,7 @@ def main():
         for agent in blue_agent_list:
             blue_obs[agent] = observations[agent]
 
-        if i % SHOW_INTERVAL == 0:
+        if i % hyperparams['render_interval'] == 0:
             env.show = True
             env.start_recording(f'{FOLDER}/training_vids/{i}.mp4')
 
@@ -124,10 +159,10 @@ def main():
 
             red_team.memory.store_transition(red_obs, red_actions, red_rewards, red_obs_, red_dones)
 
-            if steps % LEARN_INTERVAL == 0 and steps > 0:
+            if steps % hyperparams['learn_interval'] == 0 and steps > 0:
                 red_team.learn()
 
-            if steps % SAVE_INTERVAL == 0 and steps > 0:
+            if steps % hyperparams['save_interval'] == 0 and steps > 0:
                 red_team.save_models()
 
             red_obs = red_obs_
@@ -137,19 +172,19 @@ def main():
             blue_scores.append(blue_score)
             steps += 1
 
-        if i % PRINT_INTERVAL == 0 and i > 0:
+        if i % hyperparams['print_interval'] == 0 and i > 0:
             now = datetime.datetime.now()
             elapsed = now - start
             s = elapsed.total_seconds()
             hours, remainder = divmod(s, 3600)
             minutes, seconds = divmod(remainder, 60)
             formatted_elapsed = f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
-            formatted_time = f"{now.hour}:{now.minute}:{now.second}"
-            avg_red = np.mean(red_scores[-PRINT_INTERVAL:])
-            avg_blue = np.mean(blue_scores[-PRINT_INTERVAL:])
+            formatted_time = now.strftime("%I:%M:%S %p")
+            avg_red = np.mean(red_scores[-hyperparams['print_interval']:])
+            avg_blue = np.mean(blue_scores[-hyperparams['print_interval']:])
             statement = (
                 f"\n{'-'*43}\n"
-                f"| {('Current Time: ' + str(formatted_time)):<40}|\n"
+                f"| {('Current Time: ' + formatted_time):<40}|\n"
                 f"| {('Elapsed Time: ' + str(formatted_elapsed)):<40}|\n"
                 f"| {('Games: ' + str(i)):<40}|\n"
                 f"| {('Timesteps: ' + str(steps)):<40}|\n"
@@ -165,6 +200,3 @@ def main():
     }
 
     plot_data(score_dict, FOLDER + '/scores.svg')
-
-if __name__ == '__main__':
-    main()
