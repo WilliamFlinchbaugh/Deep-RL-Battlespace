75417b5151cd22346ad32bd7a7f74a13e3d5c15a
BallpointPenBoi <williamflinchbaugh@gmail.com>
2022-12-18 15:38:41 -0600

    slightly buffed instinct agent

--- instinct/agent.py
+++ instinct/agent.py
@@ -8,9 +8,14 @@ class InstinctAgent:
         self.env = env
 
     def choose_action(self, observation):
+        # Collect the observations and calculate the actual data
         enemy_info = {}
+
+        # Base info
         enemy_info['base_dist'] = (observation[0] + 1) / 2 * (math.sqrt(math.pow(self.env.width, 2) + math.pow(self.env.height, 2)))
         enemy_info['base_angle'] = observation[1] * 360
+
+        # Enemy info
         index = 2
         for enemy in self.enemy_list:
             enemy_info[f"{enemy}_alive"] = observation[index]
@@ -18,6 +23,7 @@ class InstinctAgent:
             enemy_info[f"{enemy}_angle"] = observation[index+2] * 360
             index += 3
         
+        # Calculate the "scores" for each enemy to find target
         enemy_scores = []
         enemy_scores.append(enemy_info[f"base_dist"] * abs(enemy_info[f"base_angle"]))
         for enemy in self.enemy_list:
@@ -26,26 +32,28 @@ class InstinctAgent:
             else:
                 enemy_scores.append(1000000) # if dead, set to a very high value
 
+        # Choose the target
         if min(enemy_scores) == enemy_scores[0]:
             target = "base"
         else:
             target = self.enemy_list[enemy_scores.index(min(enemy_scores)) - 1]
 
-        if self.env.continuous_actions:
+        if self.env.continuous_actions: # If continuous actions
             actions = [0, 0, 0]
-            if enemy_info[f"{target}_dist"] < self.env.shot_dist / 2 and abs(enemy_info[f"{target}_angle"]) < 20:
-                actions[2] = 1 if np.random.rand() > 0.5 else -1 # shoot if it passes the 50/50
-            actions[0] = enemy_info[f"{target}_dist"] / math.sqrt(math.pow(self.env.width, 2) + math.pow(self.env.height, 2)) * 2 - 1
-            if enemy_info[f"{target}_angle"] > 0:
+            if enemy_info[f"{target}_dist"] < self.env.shot_dist / 3 * 2 and abs(enemy_info[f"{target}_angle"]) < 20: # If within 2/3 of shot distance and within 20 degrees
+                actions[2] = 1 if np.random.rand() > 0.6 else -1 # shoot if it passes the 60/40
+            actions[0] = enemy_info[f"{target}_dist"] / math.sqrt(math.pow(self.env.width, 2) + math.pow(self.env.height, 2)) * 2 - 1 # Calculate speed based on distance to target
+            if enemy_info[f"{target}_angle"] > 0: # If aiming to the right of target, turn left
                 actions[1] = max(-enemy_info[f"{target}_angle"] / self.env.max_turn, -1)
-            else:
+            else: # If aiming to the left of target, turn right
                 actions[1] = min(-enemy_info[f"{target}_angle"] / self.env.max_turn, 1)
             
-            noise = np.random.uniform(-0.2, 0.2, size=3)
+            noise = np.random.uniform(-0.15, 0.15, size=3) # Add a small bit of noise to the actions
             actions = np.clip(actions + noise, -1, 1)
 
             return actions
-        else:
+
+        else: # If discrete actions
             if enemy_info[f"{target}_dist"] < self.env.shot_dist / 2 and abs(enemy_info[f"{target}_angle"]) < 20:
                 return 1
             if enemy_info[f"{target}_angle"] > 0:
