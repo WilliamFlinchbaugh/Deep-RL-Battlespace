82273aee01ec2100df44ca1b0bb5015c919e29f9
BallpointPenBoi <williamflinchbaugh@gmail.com>
2022-12-19 19:10:27 -0600

    fixed 60% chance

--- instinct/agent.py
+++ instinct/agent.py
@@ -41,7 +41,7 @@ class InstinctAgent:
         if self.env.continuous_actions: # If continuous actions
             actions = [0, 0, 0]
             if enemy_info[f"{target}_dist"] < self.env.shot_dist / 3 * 2 and abs(enemy_info[f"{target}_angle"]) < 20: # If within 2/3 of shot distance and within 20 degrees
-                actions[2] = 1 if np.random.rand() > 0.6 else -1 # shoot if it passes the 60/40
+                actions[2] = 1 if np.random.rand() < 0.6 else -1 # shoot if it passes the 60/40
             actions[0] = enemy_info[f"{target}_dist"] / math.sqrt(math.pow(self.env.width, 2) + math.pow(self.env.height, 2)) * 2 - 1 # Calculate speed based on distance to target
             if enemy_info[f"{target}_angle"] > 0: # If aiming to the right of target, turn left
                 actions[1] = max(-enemy_info[f"{target}_angle"] / self.env.max_turn, -1)
